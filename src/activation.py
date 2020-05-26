df418c0977e249948a8dde5bd9e37d6041ab1487
oratta <oratta@gmail.com>
2020-05-27 08:31:25 +0900

    softmax

--- src/activation.py
+++ src/activation.py
@@ -11,3 +11,11 @@ def relu(x):
 
 def identity_function(x):
     return x
+
+def softmax(a):
+    c = np.max(a);
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
