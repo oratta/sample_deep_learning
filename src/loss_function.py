b607ce1f324746eaea3f93c63baaeddc4a7be81b
oratta <oratta@gmail.com>
2020-06-01 16:05:06 +0900

    correspond cross entropy error to batch

--- src/loss_function.py
+++ src/loss_function.py
@@ -4,5 +4,9 @@ def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y+delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y+1e-7)) / batch_size
