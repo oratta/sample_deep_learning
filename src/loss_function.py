09ed412b1ca379bacfe2b2a88af135b2cdb959cf
oratta <oratta@gmail.com>
2020-06-12 11:47:49 +0900

    numerical gradient

--- src/loss_function.py
+++ src/loss_function.py
@@ -14,3 +14,22 @@ def cross_entropy_error(y, t):
 def numerical_diff(f, x):
     h = 1e-4 #0.0001
     return (f(x+h) - f(x-h)) / (2*h)
+
+def numerical_gradient(f, x):
+    h = 1e-4 #0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        #f(x+h)
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        #f(x-h)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
