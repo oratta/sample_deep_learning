a0cff71449b30952e2400de180ecc51665a94076
oratta <oratta@gmail.com>
2020-06-12 11:39:24 +0900

    numerical diff

--- src/loss_function.py
+++ src/loss_function.py
@@ -10,3 +10,7 @@ def cross_entropy_error(y, t):
 
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y+1e-7)) / batch_size
+
+def numerical_diff(f, x):
+    h = 1e-4 #0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
