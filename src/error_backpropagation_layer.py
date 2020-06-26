3db448eb61537aefa7db06af2876005f35da8941
oratta <oratta@gmail.com>
2020-06-26 10:23:29 +0900

    implemented SoftmaxWithLoss

--- src/error_backpropagation_layer.py
+++ src/error_backpropagation_layer.py
@@ -1,4 +1,8 @@
+import sys, os
 import numpy as np
+sys.path.append(os.pardir)
+from src.loss_function import *
+from src.activation import *
 
 class MulLayer:
     def __init__(self):
@@ -83,3 +87,22 @@ class Affine:
 
         return dx
 
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
+    
